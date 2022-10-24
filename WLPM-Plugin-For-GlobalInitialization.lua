8b8e1d6bb19bd6bb08b56915f6c74bc6d1560027
Bribe <71581275+BribeFromTheHive@users.noreply.github.com>
2022-10-24 22:16:58 +0200

    Create WLPM-Plugin-For-GlobalInitialization.lua
    Just for cross-compatibility.

---
+++ WLPM-Plugin-For-GlobalInitialization.lua
@@ -0,0 +1,57 @@
+-- project: Warcraft 3 Lua Package Manager
+-- author: ScorpioT1000 / scorpiot1000@yandex.ru / github.com/indaxia
+-- Re-tooled to work seamlessly with Global Initialization v4.4.
+do
+    local exportMap = {}
+
+    ---Import a module. If it is not yet declared, yield the thread until it is. If a second parameter is specified,
+    ---then the first parameter needs to be a specific component of that module. Compare to JavaScript's "import {useState} from 'react'".
+    ---@param whichModule string
+    ---@param actualModule? string
+    function importWM(whichModule, actualModule)
+        local result = Require(actualModule or whichModule)
+        if actualModule and exportMap[actualModule] then
+            return exportMap[actualModule][whichModule]
+        end
+        return result
+    end
+    ---Declare a module and a function to be called to initialize it.
+    ---@param name string
+    ---@param imports? table
+    ---@param initFunc fun(import:importWM, export:fun(contents:table|string, value?:string), exportDefault:fun(value:any))
+    function WM(name, imports, initFunc)
+        local userFunc
+        if initFunc then
+            userFunc = initFunc
+            imports = imports or {}
+        else
+            userFunc,imports = imports,{}
+        end
+        if type(userFunc)~="function" then
+            (ThrowError or print)("Invalid initializer provided to: "..name.."; function expected, got "..type(userFunc))
+            return
+        end
+        imports.name = name
+        local function exports(export, value)
+            local map = exportMap[name]
+            if not map then
+                map = {}
+                exportMap[name] = map
+            end
+            if type(export) == "table" then
+                for k,v in pairs(export) do map[k] = v end -- merge exported table into existing table (needed for sequential exporting calls from one module).
+            elseif type(export) == "string" then
+                map[export] = value -- a simple one-line export.
+            else
+                (ThrowError or print)("WLPM Error: wrong export syntax in module '" .. name .. "'. Use export() with a single object arg or key-value args")
+                return
+            end
+        end
+        OnLibraryInit(imports, function()
+            userFunc(importWM, exports, function(val) rawset(_G, name, val) end)
+        end)
+    end
+    --Not needed, but exist for supporting the whole API:
+    loadAllWMs , wlpmLoadAllModules = DoNothing, DoNothing
+    wlpmDeclareModule , wlpmImportModule , wlpmLoadModule = WM, importWM, importWM
+end
