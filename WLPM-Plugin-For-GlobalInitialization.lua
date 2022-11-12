334f98f217e1d6b5bdca5448192599cc5c8e56cf
Bribe <71581275+BribeFromTheHive@users.noreply.github.com>
2022-11-12 14:09:19 +0100

    Update WLPM-Plugin-For-GlobalInitialization.lua

--- WLPM-Plugin-For-GlobalInitialization.lua
+++ WLPM-Plugin-For-GlobalInitialization.lua
@@ -1,39 +1,56 @@
---Global Initialization 'Lite' by Bribe, with special thanks to Tasyen and Eikonium
---Last updated 11 Nov 2022
+-- project: Warcraft 3 Lua Package Manager
+-- author: ScorpioT1000 / scorpiot1000@yandex.ru / github.com/indaxia
+-- Re-tooled to work seamlessly with Total Initialization v5.
 do
-    local addInit
-    function OnGlobalInit(initFunc) addInit("InitGlobals",               initFunc) end -- Runs once all GUI variables are instantiated.
-    function OnTrigInit  (initFunc) addInit("InitCustomTriggers",        initFunc) end -- Runs once all InitTrig_ are called.
-    function OnMapInit   (initFunc) addInit("RunInitializationTriggers", initFunc) end -- Runs once all Map Initialization triggers are run.
-    function OnGameStart (initFunc) addInit("MarkGameStarted",           initFunc) end -- Runs once the game has actually started.
-    do
-        local initializers = {}
-        addInit=function(initName, initFunc)
-            initializers[initName] = initializers[initName] or {}
-            table.insert(initializers[initName], initFunc)
+    local exportMap = {}
+
+    ---Import a module. If it is not yet declared, yield the thread until it is. If a second parameter is specified,
+    ---then the first parameter needs to be a specific component of that module. Compare to JavaScript's "import {useState} from 'react'".
+    ---@param whichModule string
+    ---@param actualModule? string
+    function importWM(whichModule, actualModule)
+        local result = Require(actualModule or whichModule)
+        if actualModule and exportMap[actualModule] then
+            return exportMap[actualModule][whichModule]
         end
-        local function init(initName, continue)
-            if initializers[initName] then
-                for _,initFunc in ipairs(initializers[initName]) do pcall(initFunc) end
-            end
-            if continue then continue() end
-        end
-        local function hook(name, continue)
-            local _name=rawget(_G, name)
-            if _name then
-                rawset(_G, name, function()
-                    _name()
-                    init(name, continue) --run the initializer after the hooked handler function has been called.
-                end)
-            else
-                init(name, continue) --run initializer immediately
-            end
-        end
-        hook("InitGlobals",function()
-            hook("InitCustomTriggers",function() --InitCustomTriggers and RunInitializationTriggers are declared after the users' code,
-                hook "RunInitializationTriggers" --hence users need to wait until they have been declared.
-            end)
-        end)
-        hook "MarkGameStarted"
+        return result
     end
+    ---Declare a module and a function to be called to initialize it.
+    ---@param name string
+    ---@param imports? table
+    ---@param initFunc fun(import:importWM, export:fun(contents:table|string, value?:string), exportDefault:fun(value:any))
+    function WM(name, imports, initFunc)
+        local userFunc
+        if initFunc then
+            userFunc = initFunc
+            imports = imports or {}
+        else
+            userFunc,imports = imports,{}
+        end
+        if type(userFunc)~="function" then
+            error("Invalid initializer provided to: "..name.."; function expected, got "..type(userFunc))
+        end
+        imports.name = name
+        local function exports(export, value)
+            local map = exportMap[name]
+            if not map then
+                map = {}
+                exportMap[name] = map
+            end
+            if type(export) == "table" then
+                for k,v in pairs(export) do map[k] = v end -- merge exported table into existing table (needed for sequential exporting calls from one module).
+            elseif type(export) == "string" then
+                map[export] = value -- a simple one-line export.
+            else
+                error("WLPM Error: wrong export syntax in module '" .. name .. "'. Use export() with a single object arg or key-value args")
+            end
+        end
+        OnInit(function()
+            Require(imports)
+            userFunc(importWM, exports, function(val) rawset(_G, name, val) end)
+        end)
+    end
+    --Not needed, but exist for supporting the whole API:
+    loadAllWMs , wlpmLoadAllModules = DoNothing, DoNothing
+    wlpmDeclareModule , wlpmImportModule , wlpmLoadModule = WM, importWM, importWM
 end
